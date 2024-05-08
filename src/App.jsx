9e39e8d8d3d327f636b3c867d19d13ff959a762e
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-05-08 10:45:29 +0200

    created hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Features from "./components/Features";
+import Testimonials from "./components/Testimonials";
+import CtaSection from "./components/CtaSection";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  Aos.init({
+    duration: 1800,
+    offset: 0,
+  });
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="overflow-hidden">
+      <Hero />
+    </div>
+  );
 }
 
-export default App
+export default App;
